c26b0ad63b8619d6325f572b68921b44fd6b9a0f
Om Prakash <117626285+OMIIIEE@users.noreply.github.com>
2023-08-21 15:09:30 +0530

    Update App.js
    added funtionality

--- src/App.js
+++ src/App.js
@@ -8,46 +8,47 @@ import { BrowserRouter as Router, Switch, Route, BrowserRouter} from "react-rout
 
 export default class App extends Component {
   pageSize= 10;
+  country="in"
   
   render() {
     return (
       
       <div>
-        <BrowserRouter>
+        <BrowserRouter Basename={process.env.PUBLIC_URL}>
           <Navbar />
 
           <Switch>
-            <Route exact path="/">
+            <Route exact path="/NEWSKART">
               {" "}
-              <News key="general" pageSize={this.pageSize} country="in" category="general" />
+              <News key="general" pageSize={this.pageSize} country={this.country} category="general" />
             </Route>
             <Route exact path="/business">
               {" "}
-              <News key="business" pageSize={this.pageSize} country="in" category="business" />
+              <News key="business" pageSize={this.pageSize} country={this.country} category="business" />
             </Route>
             <Route exact path="/entertainment">
               {" "}
-              <News key=" entertainment" pageSize={this.pageSize} country="in" category="entertainment" />
+              <News key=" entertainment" pageSize={this.pageSize} country={this.country} category="entertainment" />
             </Route>
             <Route exact path="/general">
               {" "}
-              <News key=" general" pageSize={this.pageSize} country="in" category="general" />
+              <News key=" general" pageSize={this.pageSize} country={this.country} category="general" />
             </Route>
             <Route exact path="/health">
               {" "}
-              <News key=" health" pageSize={this.pageSize} country="in" category="health" />
+              <News key=" health" pageSize={this.pageSize} country={this.country} category="health" />
             </Route>
             <Route exact path="/science">
               {" "}
-              <News key="science " pageSize={this.pageSize} country="in" category="science" />
+              <News key="science " pageSize={this.pageSize} country={this.country} category="science" />
             </Route>
             <Route exact path="/sports">
               {" "}
-              <News key="sports " pageSize={this.pageSize} country="in" category="sports" />
+              <News key="sports " pageSize={this.pageSize} country={this.country} category="sports" />
             </Route>
             <Route exact path="/technology">
               {" "}
-              <News key=" technology" pageSize={this.pageSize} country="in" category="technology" />
+              <News key=" technology" pageSize={this.pageSize} country={this.country} category="technology" />
             </Route>
           </Switch>
           </BrowserRouter>
